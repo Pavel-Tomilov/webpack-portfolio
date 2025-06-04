7225e281c84daa5fde9502ba39bd96c7bea239d5
Pavel <pasha.tomiloff@gmail.com>
2025-06-04 16:25:07 +0300

    ico

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,33 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
  context: path.resolve(__dirname, 'src'),
     mode: 'development',
   entry: {
     main: './js/main.js',
-    
   },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+  }
+},
+devServer: {
+  static: {
+    directory: path.join(__dirname, 'dist'), // откуда раздавать файлы
+  },
+  port: 3000, // порт (можно любой)
+  open: true, // автоматически открывать браузер
+},
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
@@ -17,7 +36,15 @@ module.exports = {
     new HTMLWebpackPlugin({
          template: './index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist/favicon.ico')  // Указываем полный путь с именем файла
+        }
+      ]
+    })
   ] ,
    module: {
     rules: [
